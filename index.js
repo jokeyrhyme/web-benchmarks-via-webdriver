e1aa3cea8fab9bd340d0ff73527372624c603a69
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-09 18:51:14 +1000

    add ECMAScript's Test262

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ var async = require('async');
 // this module
 
 async.series([
+  // standards compliance
   require('./lib/benchmarks/html5test'),
   require('./lib/benchmarks/css3test'),
   require('./lib/benchmarks/kangax-es6'),
   require('./lib/benchmarks/kangax-es7'),
+  require('./lib/benchmarks/test262'),
+  // javascript performance
   require('./lib/benchmarks/octane')
 ]);
