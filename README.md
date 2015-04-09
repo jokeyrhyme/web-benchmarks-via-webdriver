28b00e3775de7dfe68737ecffc9acb355fa7e9e7
Ron Waldon <jokeyrhyme@gmail.com>
2015-04-09 21:33:11 +1000

    run tests in Chrome as well as Firefox

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # web benchmarks via webdriver
 
 run popular web browser / webview benchmarks using webdriver
+
+## prequisites
+
+### optional drivers
+
+You need these drivers if you want to see results from the matching browser.
+
+- PhantomJS: included
+- Firefox: included
+- Chrome: http://chromedriver.storage.googleapis.com/index.html
+- IE and Safari: http://selenium-release.storage.googleapis.com/index.html
--- index.js
+++ index.js
@@ -6,7 +6,12 @@ var async = require('async');
 
 // this module
 
-async.series([
+var browsers = [
+  'chrome',
+  'firefox'
+];
+
+var tests = [
   // standards compliance
   require('./lib/benchmarks/html5test'),
   require('./lib/benchmarks/css3test'),
@@ -15,4 +20,14 @@ async.series([
   require('./lib/benchmarks/test262'),
   // javascript performance
   require('./lib/benchmarks/octane')
-]);
+];
+
+var tasks = [];
+
+browsers.forEach(function (browser) {
+  return tests.forEach(function (test) {
+    tasks.push(test(browser));
+  });
+});
+
+async.series(tasks);
